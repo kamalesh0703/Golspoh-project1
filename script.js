cc4ccbef1b08045272fcb4b7460f91846de3053b
kamalesh0703 <kamaleshponmani0703@gmail.com>
2023-11-27 15:04:00 +0530

    01

--- script.js
+++ script.js
@@ -5,4 +5,29 @@ function linkAction(){
     this.classList.add('active')
 }
 
-list.forEach(n=>n.addEventListener('click',linkAction))
+list.forEach(n=>n.addEventListener('click',linkAction));
+
+const menu=document.querySelector('.menu-btn');
+const nav_list=document.querySelector('.nav-lists')
+
+menu.addEventListener('click',()=>{
+   if( nav_list.style.right === "0%"){
+    nav_list.style.right="-75%";
+   }
+   else{
+    nav_list.style.right="0%";
+   }
+})
+
+const navlink=document.querySelectorAll('.nav-link');
+
+function navaction(){
+    if( nav_list.style.right === "0%"){
+        nav_list.style.right="-75%";
+    }
+    else{
+        nav_list.style.right="0%";
+    }
+}
+
+navlink.forEach(n=>n.addEventListener('click',navaction))
